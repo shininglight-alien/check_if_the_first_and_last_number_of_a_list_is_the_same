612f0dfaa62a3180d9bffe15193b5c2c32dd65b5
Ellaine Rivera <ellainejoycerrivera@iskolarngbayan.pup.edu.ph>
2024-01-25 11:01:15 +0800

    for revision

--- check_if_the_first_and_last_number_of_a_list_is_the_same.py
+++ check_if_the_first_and_last_number_of_a_list_is_the_same.py
@@ -10,7 +10,7 @@ numbers_x = [10, 20, 30, 40, 10]
 numbers_y = [75, 65, 35, 75, 30]
 
 first_num, last_num, result = check_first_last(numbers_x)
-print(f"First Number: {first_num}, Last Number: {last_num}, Same: {result}")
+print(f"For the Given x = {numbers_x}, First Number: {first_num}, Last Number: {last_num}, Same: {result}")
 
 first_num, last_num, result = check_first_last(numbers_y)
-print(f"First Number: {first_num}, Last Number: {last_num}, Same: {result}")
+print(f"For the Given y = {numbers_y},First Number: {first_num}, Last Number: {last_num}, Same: {result}")
